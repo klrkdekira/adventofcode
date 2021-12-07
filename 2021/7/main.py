914819f55f9c7b8bf05bdc2178b43bcb64e810d1
Chee Leong <klrkdekira@gmail.com>
2021-12-07 14:12:59 +0800

    chore: optimise for sake of number

--- 2021/7/main.py
+++ 2021/7/main.py
@@ -1,41 +1,40 @@
+from functools import cache
+
 crabs = []
 with open('input') as file:
     crabs = list(map(int, next(file).strip().split(',')))
 
 
-lower = 0
-for i in range(len(crabs)):
-    p = i + 1
-    total = 0
-    for crab in crabs:
-        total += abs(crab - p)
-
-    if lower == 0:
-        lower = total
-
-    if total < lower:
-        lower = total
-
-print('part 1')
-print(lower)
-
-
+@cache
 def sum_of_seq(n):
     return int((n * (n+1))/2)
 
 
-lower = 0
+lowest1 = None
+lowest2 = None
 for i in range(len(crabs)):
     p = i + 1
-    total = 0
+
+    total1 = 0
+    total2 = 0
     for crab in crabs:
-        total += sum_of_seq(abs(crab - p))
+        steps = abs(crab - p)
+        total1 += steps
+        total2 += sum_of_seq(steps)
 
-    if lower == 0:
-        lower = total
+    if not lowest1:
+        lowest1 = total1
 
-    if total < lower:
-        lower = total
+    if not lowest2:
+        lowest2 = total2
 
+    if lowest1 > total1:
+        lowest1 = total1
+
+    if lowest2 > total2:
+        lowest2 = total2
+
+print('part 1')
+print(lowest1)
 print('part 2')
-print(lower)
+print(lowest2)
