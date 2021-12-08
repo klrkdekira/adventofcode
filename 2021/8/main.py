f01c16546851536efbd61d0b54f22af5fd0774cd
Chee Leong <klrkdekira@gmail.com>
2021-12-08 21:43:26 +0800

    feat: cleanup 2021 day 8

--- 2021/8/main.py
+++ 2021/8/main.py
@@ -2,112 +2,60 @@ entries = []
 
 
 def parse(line):
-    entry = []
-    for part in line.strip().split("|"):
-        entry.append(
-            tuple(map(lambda x: "".join(sorted(x.strip())), part.strip().split(" ")))
-        )
-    return entry
+    return list(map(lambda x: tuple(x.strip().split(' ')), line.strip().split('|')))
 
 
 with open("input") as file:
     entries = list(map(parse, file))
 
-panel = {
-    0: "abcefg",
-    1: "cf",
-    2: "acdeg",
-    3: "acdfg",
-    4: "bcdf",
-    5: "abdfg",
-    6: "abdefg",
-    7: "acf",
-    8: "abcdefg",
-    9: "abcdfg",
-}
-
-panel_lookup = {v: k for k, v in panel.items()}
-
-outputs = list(map(lambda x: x[1], entries))
-
-target = [
-    len(panel[1]),
-    len(panel[4]),
-    len(panel[7]),
-    len(panel[8]),
-]
-
 count = 0
-for output in outputs:
-    for value in output:
-        if len(value) in target:
-            count += 1
-
-print("part 1")
-print(count)
-
 total = 0
-for segments, output in entries:
-    d = {}
+for segments, outputs in entries:
+    dict = {}
 
-    rest = []
+    for segment in segments:
+        length = len(segment)
+        if length == 2:
+            dict[1] = segment
+        elif length == 4:
+            dict[4] = segment
+        elif length == 3:
+            dict[7] = segment
+        elif length == 7:
+            dict[8] = segment
 
-    for s in segments:
-        l = len(s)
-        if l == 2:
-            d[1] = s
-        elif l == 4:
-            d[4] = s
-        elif l == 3:
-            d[7] = s
-        elif l == 7:
-            d[8] = s
-        else:
-            rest.append(s)
-
-    while len(rest) > 0:
-        for s in rest:
-            if len(s) == 6:
-                matched = 0
-                for i in [1, 4, 7]:
-                    if len(set(s).intersection(d[i])) == len(d[i]):
-                        matched += 1
-                if matched == 0:
-                    d[6] = s
-                    rest.remove(s)
-                elif matched == 2:
-                    d[0] = s
-                    rest.remove(s)
-                elif matched == 3:
-                    d[9] = s
-                    rest.remove(s)
-            elif len(s) == 5:
-                matched = 0
-                for i in [1, 7]:
-                    if len(set(s).intersection(d[i])) == len(d[i]):
-                        matched += 1
-
-                if matched == 2:
-                    d[3] = s
-                    rest.remove(s)
-                    continue
-
-                if 6 in d:
-                    if len(set(s).intersection(d[6])) == len(d[6]) - 1:
-                        d[5] = s
-                        rest.remove(s)
-                        continue
-                    else:
-                        d[2] = s
-                        rest.remove(s)
-                        continue
-
-    reversed = {v: str(k) for k, v in d.items()}
-
-    num = ""
-    for o in output:
-        num += reversed[o]
+    num = ''
+    for output in outputs:
+        length = len(output)
+        if length == 2:
+            num += '1'
+            count += 1
+        elif length == 4:
+            num += '4'
+            count += 1
+        elif length == 3:
+            num += '7'
+            count += 1
+        elif length == 7:
+            num += '8'
+            count += 1
+        elif length == 5:
+            if len(set(output).intersection(dict[1])) == 2:
+                num += '3'
+            elif len(set(output).intersection(dict[4])) == 2:
+                num += '2'
+            else:
+                num += '5'
+        elif length == 6:
+            if len(set(output).intersection(dict[1])) == 1:
+                num += '6'
+            elif len(set(output).intersection(dict[4])) == 4:
+                num += '9'
+            else:
+                num += '0'
     total += int(num)
 
+print('part 1')
+print(count)
 print("part 2")
 print(total)
