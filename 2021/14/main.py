f5c72b3733abbece4765759c9d346a80dfefd2f1
Chee Leong <klrkdekira@gmail.com>
2021-12-14 15:13:58 +0800

    feat: use defaultdict

--- 2021/14/main.py
+++ 2021/14/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 raw_template = []
 rules = []
 
@@ -7,68 +9,42 @@ with open("input") as file:
         line = line.strip()
         if not line:
             continue
-
         left, right = line.split("->")
         left = left.strip()
         right = right.strip()
         rules.append(((left[0], left[1]), right))
     rules = dict(rules)
 
-template = {}
+template = defaultdict(int)
 for i, v in enumerate(raw_template):
     if i == 0:
         continue
-
     key = raw_template[i - 1], v
     if key in rules:
-        if key not in template:
-            template[key] = 0
         template[key] += 1
 
 
 def insertion(polymer):
-    changes = {}
+    changes = defaultdict(int)
     for k, v in polymer.items():
         if v == 0:
             continue
-
         if k in rules:
             left = k[0], rules[k]
             right = rules[k], k[1]
-
-            if k not in changes:
-                changes[k] = 0
-
-            if left not in changes:
-                changes[left] = 0
-
-            if right not in changes:
-                changes[right] = 0
-
             changes[k] -= v
             changes[left] += v
             changes[right] += v
-
     for k, v in changes.items():
-        if k not in polymer:
-            polymer[k] = 0
         polymer[k] += v
 
 
 def summarise(polymer):
-    summary = {}
+    summary = defaultdict(int)
     for k, v in polymer.items():
         a, b = k
-
-        if a not in summary:
-            summary[a] = 0
-
-        if b not in summary:
-            summary[b] = 0
-
         summary[a] += v / 2
         summary[b] += v / 2
-
     values = sorted(summary.values())
     return int(values[len(values) - 1] - values[0])
 
